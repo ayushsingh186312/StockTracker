47b0a6d5ed3437ca6fccc2a0213948cb64267f3f
Ayush Singh <124523646+ayushsingh186312@users.noreply.github.com>
2024-12-27 02:02:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(express.json());
 
 // MongoDB Connection String (Replace `<username>`, `<password>`, and `<cluster-url>` with your Atlas details)
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://ayushsingh186312:h0MMXqdc1jobJb9x@cluster0.1bxn4.mongodb.net/";
+const uri = "mongodb+srv://ayushsingh186312:h0MMXqdc1jobJb9x@cluster0.1bxn4.mongodb.net/shares";
 
 // Connect to MongoDB Atlas
 console.log("Connected to MongoDB Atlas");
